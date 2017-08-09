f545027ab503e00cce3fb0899b8bac5fcc44e95d
kavdihodgson <kavdyjh@gmail.com>
2017-08-09 15:50:06 -0700

    updated hw file

--- app.js
+++ app.js
@@ -1,47 +1,62 @@
-'use strict';
+// 'use strict';
+//
+// alert ('Hey there! Lets play a small guessing game so you can get to know me a little better! Please respond with Yes or No answer s.');
+// var a = 'I have visited more than 12 different countries.';
+// var ask = prompt(a);
+// var res = ask.toUpperCase();
+// console.log (res);
+// if (res === 'YES' || res === 'Y') {alert ('Correct!');}
+// else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
+// else {alert ('YES OR NO ANSWERS ONLY!');}
+// console.log (res);
+//
+// var a = 'Spanish is my first language.';
+// var ask = prompt(a);
+// var res = ask.toUpperCase();
+// console.log (res);
+// if (res === 'YES' || res === 'Y') {alert ('Correct!');}
+// else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
+// else {alert ('YES OR NO ANSWERS ONLY!');}
+// console.log (res);
+//
+// var a = 'Basketball is my favorite sport.';
+// var ask = prompt(a);
+// var res = ask.toUpperCase();
+// console.log (res);
+// if (res === 'NO' || res === 'N') {alert ('Correct!');}
+// else if (res === 'YES' || res === 'Y') {alert ('You are incorrect.');}
+// else {alert ('YES OR NO ANSWERS ONLY!');}
+// console.log (res);
+//
+// var a = 'I have an addiction to watches.';
+// var ask = prompt(a);
+// var res = ask.toUpperCase();
+// console.log (res);
+// if (res === 'YES' || res === 'Y') {alert ('Correct!');}
+// else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
+// else {alert ('YES OR NO ANSWERS ONLY!');}
+// console.log (res);
+//
+// var a = 'I am afraid of heights.';
+// var ask = prompt(a);
+// var res = ask.toUpperCase();
+// console.log (res);
+// if (res === 'NO' || res === 'N') {alert ('Correct!');}
+// else if (res === 'YES' || res === 'Y') {alert ('You are incorrect.');}
+// else {alert ('YES OR NO ANSWERS ONLY!');}
+// console.log (res);
 
-alert ('Hey there! Lets play a small guessing game so you can get to know me a little better! Please respond with Yes or No answer s.');
-var a = 'I have visited more than 12 different countries.';
-var ask = prompt(a);
-var res = ask.toUpperCase();
-console.log (res);
-if (res === 'YES' || res === 'Y') {alert ('Correct!');}
-else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
-else {alert ('YES OR NO ANSWERS ONLY!');}
-console.log (res);
-
-var a = 'Spanish is my first language.';
-var ask = prompt(a);
-var res = ask.toUpperCase();
-console.log (res);
-if (res === 'YES' || res === 'Y') {alert ('Correct!');}
-else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
-else {alert ('YES OR NO ANSWERS ONLY!');}
-console.log (res);
-
-var a = 'Basketball is my favorite sport.';
-var ask = prompt(a);
-var res = ask.toUpperCase();
-console.log (res);
-if (res === 'NO' || res === 'N') {alert ('Correct!');}
-else if (res === 'YES' || res === 'Y') {alert ('You are incorrect.');}
-else {alert ('YES OR NO ANSWERS ONLY!');}
-console.log (res);
-
-var a = 'I have an addiction to watches.';
-var ask = prompt(a);
-var res = ask.toUpperCase();
-console.log (res);
-if (res === 'YES' || res === 'Y') {alert ('Correct!');}
-else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
-else {alert ('YES OR NO ANSWERS ONLY!');}
-console.log (res);
-
-var a = 'I am afraid of heights.';
-var ask = prompt(a);
-var res = ask.toUpperCase();
-console.log (res);
-if (res === 'NO' || res === 'N') {alert ('Correct!');}
-else if (res === 'YES' || res === 'Y') {alert ('You are incorrect.');}
-else {alert ('YES OR NO ANSWERS ONLY!');}
-console.log (res);
+// var a = 'Lets play a guessing game. What is my favorite number? You get 4 tries!';
+// var answer = 22;
+// var i = 0;
+// for (var i = 0; i < 4; i++){
+//   var ask = prompt (a);
+//   console.log(ask);
+//   if (ask < 22) {
+//     alert ('You are guessing a little too low, try a higher number.');}
+//   else if (ask > 22) {
+//     alert ('You\'re guessing a little too high. Try a lower number.');}
+//   else {
+//     alert ('You are correct! Good guess!');
+//     break;}
+// }
