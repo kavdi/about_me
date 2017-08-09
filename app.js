60c8040821a65e02d65bdede5e537d516b7179ee
kavdihodgson <kavdyjh@gmail.com>
2017-08-08 17:09:52 -0700

    day 2 project

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+alert ('Hey there! Lets play a small guessing game so you can get to know me a little better! Please respond with Yes or No answeres.');
+var a = 'I have visited more than 12 different countries.';
+var ask = prompt(a);
+var res = ask.toUpperCase();
+console.log (res);
+if (res === 'YES' || res === 'Y') {alert ('Correct!');}
+else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
+else {alert ('YES OR NO ANSWERS ONLY!');}
+console.log (res);
+
+var a = 'Spanish is my first language.';
+var ask = prompt(a);
+var res = ask.toUpperCase();
+console.log (res);
+if (res === 'YES' || res === 'Y') {alert ('Correct!');}
+else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
+else {alert ('YES OR NO ANSWERS ONLY!');}
+console.log (res);
+
+var a = 'Basketball is my favorite sport.';
+var ask = prompt(a);
+var res = ask.toUpperCase();
+console.log (res);
+if (res === 'NO' || res === 'N') {alert ('Correct!');}
+else if (res === 'YES' || res === 'Y') {alert ('You are incorrect.');}
+else {alert ('YES OR NO ANSWERS ONLY!');}
+console.log (res);
+
+var a = 'I have an addiction to watches.';
+var ask = prompt(a);
+var res = ask.toUpperCase();
+console.log (res);
+if (res === 'YES' || res === 'Y') {alert ('Correct!');}
+else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
+else {alert ('YES OR NO ANSWERS ONLY!');}
+console.log (res);
+
+var a = 'I am afraid of heights.';
+var ask = prompt(a);
+var res = ask.toUpperCase();
+console.log (res);
+if (res === 'NO' || res === 'N') {alert ('Correct!');}
+else if (res === 'YES' || res === 'Y') {alert ('You are incorrect.');}
+else {alert ('YES OR NO ANSWERS ONLY!');}
+console.log (res);
