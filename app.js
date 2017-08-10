26b680cb297acd6c4cd42b16e1f71525d7b6ada4
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-08-10 14:48:05 -0700

    made first changes.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 var qlog = [];
 console.log (qlog);
 var answerlog = [];
@@ -56,7 +56,7 @@ if (res === 'NO' || res === 'N') {goodanswers++; alert ('Correct!');}
 else if (res === 'YES' || res === 'Y') {alert ('You are incorrect.');}
 else {alert ('YES OR NO ANSWERS ONLY!');}
 console.log (res);
-
+*/
 var a = 'Lets play a guessing game. What is my favorite number? You get 4 tries!';
 var answer = 22;
 for (var i = 0; i < 4; i++){
@@ -67,7 +67,8 @@ for (var i = 0; i < 4; i++){
   else if (parseInt(ask) > 22)
   { alert ('You\'re guessing a little too high. Try a lower number.');}
   else if (parseInt(ask) === 22) {
-    goodanswers++; alert ('You are correct! Good guess!');
+    //goodanswers++;
+    alert ('You are correct! Good guess!');
     break;
   }else {
     alert('That is not a number.');
@@ -76,19 +77,20 @@ for (var i = 0; i < 4; i++){
 for (i = 0; i < 6; i++) {
   var a = 'Can you guess what other countries I have lived in besides the USA?';
   console.log (a);
-  qlog.push (a);
+  //qlog.push (a);
   var countries = ['NICARAGUA','GERMANY','ARGENTINA','BRAZIL','COSTA RICA','PANAMA','BELIZE','HONDURAS','FRANCE'];
   var ask = prompt (a);
   console.log (ask);
-  answerlog.push (ask);
+  //answerlog.push (ask);
   if (countries.includes(ask.toUpperCase()))
- {alert ('Good guess! You\'re correct. ');goodanswers++;
- i += 6;
-}else if (i < (6 - 1))
- {alert ('Nope! Try again. You have' + ((6 - 1) - i) + 'turns left.');}
+ {alert ('Good guess! You\'re correct. '); //goodanswers++;
+i += 6;
+}else if (i < (6 - 1)) {
+ console.log("here!!!", i);
+ {alert ('Nope! Try again. You have' + (5 - i) + 'turns left.');}
 
-else alert ('Better luck next time.');
+}else alert ('Better luck next time.');
 }
 
-var finished = 'Thank you for playin! You got ' + goodanswers + ' out of 7 questions correct!';
-console.log (finished);
+//var finished = 'Thank you for playin! You got ' + goodanswers + ' out of 7 questions correct!';
+//console.log (finished);
