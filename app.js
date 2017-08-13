69470ca669de65f225eb983e8be8f985a390d427
kavdihodgson <kavdyjh@gmail.com>
2017-08-13 13:20:06 -0700

    minor changes

--- app.js
+++ app.js
@@ -28,69 +28,69 @@ function questionOne() {
 }
 
 function questionTwo() {
-var a = 'Spanish is my first language.';
-var ask = prompt(a);
-var res = ask.toUpperCase();
-console.log (res);
-if (res === 'YES' || res === 'Y') {goodanswers++; alert ('Correct!');}
-else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
-else {alert ('YES OR NO ANSWERS ONLY!');}
-console.log (res);
+  var a = 'Spanish is my first language.';
+  var ask = prompt(a);
+  var res = ask.toUpperCase();
+  console.log (res);
+  if (res === 'YES' || res === 'Y') {goodanswers++; alert ('Correct!');}
+  else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
+  else {alert ('YES OR NO ANSWERS ONLY!');}
+  console.log (res);
 }
 
 function questionThree() {
-var a = 'Basketball is my favorite sport.';
-var ask = prompt(a);
-var res = ask.toUpperCase();
-console.log (res);
-if (res === 'NO' || res === 'N') {
-  goodanswers++; alert ('Correct!');
-} else if (res === 'YES' || res === 'Y') {
-  alert ('You are incorrect.');
-} else {alert ('YES OR NO ANSWERS ONLY!');
-}
-console.log (res);
+  var a = 'Basketball is my favorite sport.';
+  var ask = prompt(a);
+  var res = ask.toUpperCase();
+  console.log (res);
+  if (res === 'NO' || res === 'N') {
+    goodanswers++; alert ('Correct!');
+  } else if (res === 'YES' || res === 'Y') {
+    alert ('You are incorrect.');
+  } else {alert ('YES OR NO ANSWERS ONLY!');
+  }
+  console.log (res);
 }
 
 function questionFour() {
-var a = 'I have an addiction to watches.';
-var ask = prompt(a);
-var res = ask.toUpperCase();
-console.log (res);
-if (res === 'YES' || res === 'Y') {goodanswers++; alert ('Correct!');}
-else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
-else {alert ('YES OR NO ANSWERS ONLY!');}
-console.log (res);
+  var a = 'I have an addiction to watches.';
+  var ask = prompt(a);
+  var res = ask.toUpperCase();
+  console.log (res);
+  if (res === 'YES' || res === 'Y') {goodanswers++; alert ('Correct!');}
+  else if (res === 'NO' || res === 'N') {alert ('You are incorrect.');}
+  else {alert ('YES OR NO ANSWERS ONLY!');}
+  console.log (res);
 }
 
 function questionFive() {
-var a = 'I am afraid of heights.';
-var ask = prompt(a);
-var res = ask.toUpperCase();
-console.log (res);
-if (res === 'NO' || res === 'N') {goodanswers++; alert ('Correct!');}
-else if (res === 'YES' || res === 'Y') {alert ('You are incorrect.');}
-else {alert ('YES OR NO ANSWERS ONLY!');}
-console.log (res);
+  var a = 'I am afraid of heights.';
+  var ask = prompt(a);
+  var res = ask.toUpperCase();
+  console.log (res);
+  if (res === 'NO' || res === 'N') {goodanswers++; alert ('Correct!');}
+  else if (res === 'YES' || res === 'Y') {alert ('You are incorrect.');}
+  else {alert ('YES OR NO ANSWERS ONLY!');}
+  console.log (res);
 }
 
 function questionSix() {
-var a = 'Lets play a guessing game. What is my favorite number? You get 4 tries!';
-var answer = 22;
-for (var i = 0; i < 4; i++){
-  var ask = prompt (a);
-  console.log(ask);
-  if (parseInt(ask) < 22) {
-    alert ('You are guessing a little too low, try a higher number.');}
-  else if (parseInt(ask) > 22)
+  var a = 'Lets play a guessing game. What is my favorite number? You get 4 tries!';
+  var answer = 22;
+  for (var i = 0; i < 4; i++){
+    var ask = prompt (a);
+    console.log(ask);
+    if (parseInt(ask) < 22) {
+      alert ('You are guessing a little too low, try a higher number.');}
+    else if (parseInt(ask) > 22)
   { alert ('You\'re guessing a little too high. Try a lower number.');}
-  else if (parseInt(ask) === 22) {
-    goodanswers++;
-    alert ('You are correct! Good guess!');
-    break;
-  }else {
-    alert('That is not a number.');
-  }}}
+    else if (parseInt(ask) === 22) {
+      goodanswers++;
+      alert ('You are correct! Good guess!');
+      break;
+    }else {
+      alert('That is not a number.');
+    }}}
 
 function questionSeven() {
   var i = 0;
@@ -103,13 +103,13 @@ function questionSeven() {
     console.log (ask);
     answerlog.push (ask);
     if (countries.includes(ask.toUpperCase()))
-   {alert ('Good guess! You\'re correct. '); goodanswers++;
-  i += 6;
-  }else if (i < (6 - 1)) {
-   console.log("here!!!", i);
-   {alert ('Nope! Try again. You have' + (5 - i) + 'turns left.');}
+   {alert ('Good guess! You\'re correct. ' + countries + ' , are the countries I have lived in.'); goodanswers++;
+      i += 6;
+    }else if (i < (6 - 1)) {
+      console.log('here!!!', i);
+      {alert ('Nope! Try again. You have' + (5 - i) + 'turns left.');}
 
-  }else alert ('Better luck next time.');
+    }else alert ('Better luck next time.');
   }}
 
 questionOne();
